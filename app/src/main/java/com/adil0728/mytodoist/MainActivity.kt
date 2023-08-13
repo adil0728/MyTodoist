1420db18d1351b2708f0f87fc8453ddad3ebc5fc
Adil lakkhlifi <lakxlifi@mail.ru>
2023-08-13 18:05:54 +0300

    Initial commit

---
+++ app/src/main/java/com/adil0728/mytodoist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.adil0728.mytodoist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
